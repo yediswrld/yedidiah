827ba21036e25fb8c2fd9170de55ac557848aa1c
yedidiah <onwordnwankwo@gmail.com>
2026-03-13 17:19:20 +0100

    Fix deployment files

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,3 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 ```
 
-Also create a file called `Procfile` (no extension) in the `yedidiah` folder with this:
-```
-web: cd backend && gunicorn config.wsgi:application
